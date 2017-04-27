c3e73905b42fc67669471a17c03f45e823b329ea
Dylan Avery <fossilfuelmusic@gmail.com>
2017-04-27 17:40:52 -0600

    Remove headers, sucessful API call, going to build a little front end

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<Header />
-        <Main />*/}
+        {/*<Header />*/}
+        <Main />
         <p>Hello We are finally working.</p>
       </div>
     );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,16 +17,7 @@ export default class Main extends Component {
   // }
 
   fetchWeatherForecast(){
-    fetch(`api.openweathermap.org/data/2.5/weather?q=London&APPID=6384d47538058cc99891f13f4702e409`, {
-      method: 'GET',
-      headers: {
-        'X-Xapp-Token': `${this.state.accessToken}`,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': '*'
-      }
-    })
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6384d47538058cc99891f13f4702e409`) 
     .then(response => response.json())
     .then(data => console.log(data))
   }
